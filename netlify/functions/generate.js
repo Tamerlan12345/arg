867a763caef4822e2f9e013396e3a656d62f09a4
Tamerlan12345 <centr-cool@mail.ru>
2025-09-17 14:54:30 +0500

    Update generate.js

--- netlify/functions/generate.js
+++ netlify/functions/generate.js
@@ -25,7 +25,7 @@ exports.handler = async (event, context) => {
 
     try {
         const { contents, systemInstruction, generationConfig } = JSON.parse(event.body);
-        const apiUrl = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${apiKey}`;
+        const apiUrl = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${apiKey}`;
 
         const response = await fetch(apiUrl, {
             method: 'POST',
